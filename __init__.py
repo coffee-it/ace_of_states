53764e7dc6eea9595ca2480255a7dc22ac8c50da
Sergei <sergei@coffee-it.org>
2023-01-06 00:40:12 +0300

    Cut ubus service commands to __main__.py Remote TODO's, add quotes to log messages

--- __init__.py
+++ __init__.py
@@ -1,14 +1,8 @@
 #!/usr/bin/micropython
-"""
-Варианты использования:
-- Запуск ubus сервиса;
-- импортировать как модуль для хранения локального состояния
-
-TODO разделить на эти составляющие
-"""
-import uio, btree, ubus, logging, os, errno
+import uio, btree, logging, os, errno
 from micropython import const
 
+__all__ = ['AOS_Persistant', 'AOS_Temporary']
 PERSISTANT_DB_PATH=const("/opt/ace_of_states")
 
 logging.basicConfig(level=logging.DEBUG)
@@ -43,6 +37,8 @@ class Ace_of_States_Basis():
     def low_write(self, btreeIO, variable: str, value: str) -> bool:
         try:
             btreeIO.put(variable, str(value))
+            # Flush command writing cache to file. More safety, less quickly.
+            # Way to optimize.
             btreeIO.flush()
         except Exception as e:
             log_aos.error("Low write error [%s, %s]" % (variable, value))
@@ -98,7 +94,7 @@ class Ace_of_States_Basis():
         if value >= old_value:
             value = old_value + (value - old_value)
         else:
-            log_aos.debug("Cant add. Value of %s from %s is less then current" % (variable, db_label))
+            log_aos.debug("Cant update extremum. Value of \"%s\" from \"%s\" is less then current" % (variable, db_label))
             return False
         self.write(db_label=db_label, variable=variable, value=value)
 
@@ -153,7 +149,6 @@ class Ace_of_States_Basis():
 class AOS_Persistant(Ace_of_States_Basis):
     def persistant_created(func):
         """Decorator which checks that the DB file is available"""
-        """TODO проверять наличие adj_variable и что-то с ней делать"""
         def creation_checking(self, db_label, variable, value):
             if not db_label in self.PERSISTANT_FILES:
                 if not os_exists("%s/%s" % (PERSISTANT_DB_PATH, db_label)):
@@ -167,9 +162,9 @@ class AOS_Persistant(Ace_of_States_Basis):
     def write(self, db_label, variable, value) -> bool:
         """Write to persistant storage"""
         if self.read(db_label, variable) == value:
-            log_aos.debug("[%s] Variable %s is already %s" % (db_label, variable, value))
+            log_aos.debug("[%s] Variable \"%s\" is already \"%s\"" % (db_label, variable, value))
             return True
-        log_aos.debug("[%s] Write %s to %s" % (db_label, value, variable))
+        log_aos.debug("[%s] Write \"%s\" to \"%s\"" % (db_label, value, variable))
         return self.low_write(self.PERSISTANT_FILES[db_label], variable, value)
 
     def read(self, db_label:str, variable:str, default:str = None) -> str|None:
@@ -179,7 +174,7 @@ class AOS_Persistant(Ace_of_States_Basis):
             if os_exists("%s/%s" % (PERSISTANT_DB_PATH, db_label)):
                 self.PERSISTANT_FILES.update({db_label: btree.open(self.open_db_from_file("%s/%s" % (PERSISTANT_DB_PATH, db_label)))})
             else:
-                log_aos.debug("Label %s not in PERSISTANT_FILES" % db_label)
+                log_aos.debug("Label \"%s\" not in PERSISTANT_FILES" % db_label)
                 return default
         value = self.low_read(self.PERSISTANT_FILES[db_label], variable)
         return value if value else default
@@ -187,7 +182,6 @@ class AOS_Persistant(Ace_of_States_Basis):
 class AOS_Temporary(Ace_of_States_Basis):
     def temp_created(func):
         """Decorator which checks that the IO buffer is available"""
-        """TODO проверять наличие adj_variable и что-то с ней делать"""
         def creation_checking(self, db_label, variable, value):
             if not db_label in self.TEMP_IO_STREAMS:
                 log_aos.debug("[%s] Create btree IO instance" % db_label)
@@ -200,65 +194,15 @@ class AOS_Temporary(Ace_of_States_Basis):
     def write(self, db_label:str, variable:str, value:str) -> bool:
         """Write to temporary storage"""
         if self.read(db_label, variable) == value:
-            log_aos.debug("[%s] Variable %s is already \"%s\"" % (db_label, variable, value))
+            log_aos.debug("[%s] Variable \"%s\" is already \"%s\"" % (db_label, variable, value))
             return True
-        log_aos.debug("[%s] Write %s to %s" % (db_label, value, variable))
+        log_aos.debug("[%s] Write \"%s\" to \"%s\"" % (db_label, value, variable))
         return self.low_write(self.TEMP_IO_STREAMS[db_label], variable, value)
 
     def read(self, db_label:str, variable:str, default:str = None) -> str|None:
         """Read from temporary storage"""
         if not db_label in self.TEMP_IO_STREAMS:
-            log_aos.debug("Label %s not in TEMP_IO_STREAMS" % db_label)
+            log_aos.debug("Label \"%s\" not in TEMP_IO_STREAMS" % db_label)
             return default
         value = self.low_read(self.TEMP_IO_STREAMS[db_label], variable)
         return value if value else default
-
-
-""" Ace of States ubus service"""
-Persistant_vault = AOS_Persistant()
-Temporary_vault = AOS_Temporary()
-
-def write(handler, data):
-    persistant, label, variable, value =  data['persistant'], data['label'], data['variable'], data['value']
-    if persistant:
-        res = Persistant_vault.write(label, variable, value)
-    else:
-        res = Temporary_vault.write(label, variable, value)
-    handler_reply(handler, {'Status': 'OK' if res else 'Fail'})
-
-def read(handler, data):
-    label, variable, persistant =  data['label'], data['variable'], data['persistant']
-    if persistant:
-        value = Persistant_vault.read(label, variable)
-    else:
-        value = Temporary_vault.read(label, variable)
-    handler_reply(handler, {variable: value})
-
-def drop(handler, data):
-    """DROPDATABASE"""
-    handler_reply(handler, {'Status': 'Priplily'})
-
-def handler_reply(handler, reply):
-    log_aos.debug("Reply %s" % reply)
-    handler.reply(reply)
-
-ubus.connect('/var/run/ubus.sock')
-ubus.add("aos", {
-    "write": {"method": write, "signature": {
-        "persistant": ubus.BLOBMSG_TYPE_BOOL,
-        "label": ubus.BLOBMSG_TYPE_STRING,
-        "variable":  ubus.BLOBMSG_TYPE_STRING,
-        "value":  ubus.BLOBMSG_TYPE_STRING
-        }},
-    "read": {"method": read, "signature": {
-        "persistant": ubus.BLOBMSG_TYPE_BOOL,
-        "label": ubus.BLOBMSG_TYPE_STRING,
-        "variable":  ubus.BLOBMSG_TYPE_STRING
-        }},
-    "drop": {"method": drop, "signature": {}}
-    }
-)
-try:
-    ubus.loop()
-except KeyboardInterrupt:
-    ubus.disconnect()
--- __main__.py
+++ __main__.py
@@ -1,9 +1,56 @@
-from sys import argv, stdin
-if len(argv) == 1:
-    print("No one argument given, read stdin insted")
-    for _in in stdin:
-        argv.append(_in)
+#!/usr/bin/micropython
+import ace_of_states as ace
+import ubus, logging
 
-print(argv)
-import ananas
-print(ananas.persistant_db)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("AOS Service")
+
+""" Ace of States ubus service"""
+Persistant_vault = ace.AOS_Persistant()
+Temporary_vault = ace.AOS_Temporary()
+
+def write(handler, data):
+    persistant, label, variable, value =  data['persistant'], data['label'], data['variable'], data['value']
+    if persistant:
+        res = Persistant_vault.write(label, variable, value)
+    else:
+        res = Temporary_vault.write(label, variable, value)
+    handler_reply(handler, {'Status': 'OK' if res else 'Fail'})
+
+def read(handler, data):
+    label, variable, persistant =  data['label'], data['variable'], data['persistant']
+    if persistant:
+        value = Persistant_vault.read(label, variable)
+    else:
+        value = Temporary_vault.read(label, variable)
+    handler_reply(handler, {variable: value})
+
+def drop(handler, data):
+    """DROPDATABASE"""
+    handler_reply(handler, {'Status': 'Priplily'})
+
+def handler_reply(handler, reply):
+    logger.debug("Reply %s" % reply)
+    handler.reply(reply)
+
+ubus.connect('/var/run/ubus.sock')
+ubus.add("aos", {
+    "write": {"method": write, "signature": {
+        "persistant": ubus.BLOBMSG_TYPE_BOOL,
+        "label": ubus.BLOBMSG_TYPE_STRING,
+        "variable":  ubus.BLOBMSG_TYPE_STRING,
+        "value":  ubus.BLOBMSG_TYPE_STRING
+        }},
+    "read": {"method": read, "signature": {
+        "persistant": ubus.BLOBMSG_TYPE_BOOL,
+        "label": ubus.BLOBMSG_TYPE_STRING,
+        "variable":  ubus.BLOBMSG_TYPE_STRING
+        }} #,
+    # "drop": {"method": drop, "signature": {}}
+    }
+)
+try:
+    ubus.loop()
+except KeyboardInterrupt:
+    ubus.disconnect()
+    
