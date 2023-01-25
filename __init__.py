e108ece50c25feca20f936b273b05e6265937bfe
Sergei <sergei@coffee-it.org>
2023-01-26 00:18:15 +0300

    __init__.py now offers two classes:   - Ace - to operate with btree datebase   - AOS - which be a main single ace of states node
    multiple.py
    The previous __init__.py script was transformed into multiple py, but with few changes:
      - Use an AOS node instead of direct creation btree instance
      - Use AOS methods to operate with ace of states instance
    
    __main__.py
    Remove persistant flag and split ubus handlers to two different services to interract with persistant and temporary db separately

--- __init__.py
+++ __init__.py
@@ -1,43 +1,48 @@
 #!/usr/bin/micropython
-import uio, btree, logging, os, errno
+import uio, btree, logging
+from functools import partial
+from os import path, stat
 from atsignal import SignalHandler
-from micropython import const
+from usys import atexit
 
-__all__ = ['AOS_Persistant', 'AOS_Temporary']
-PERSISTANT_DB_PATH=const("/opt/ace_of_states")
+log_aos = logging.getLogger("AOS")
 
-logging.basicConfig(level=logging.DEBUG)
-log_aos   = logging.getLogger("AOS")
+class DirNotFoundError(BaseException):
+    pass
+
+class AceError(BaseException):
+    pass
 
 def os_exists(path):
     try:
-        return os.stat(path)[0]
+        return stat(path)[0]
     except Exception as e:
         errno = e.args[0]
         if errno == errno.ENOENT:
             return 0
         raise e
 
-class Ace_of_States_Basis():
-    def __init__(self, aos_local_vault = None) -> None:
-        self.TEMP_IO_STREAMS  = {}
-        self.PERSISTANT_FILES = {}
-        if aos_local_vault:
-            self.VAULT = btree.open(self.open_db_from_file(aos_local_vault))
-        else:
-            self.VAULT = btree.open(uio.BytesIO())
-        SignalHandler.register(2, self._sync_all_persistant)
-        SignalHandler.register(15, self._sync_all_persistant)
-
-    def open_db_from_file(self, absolut_path):
-        # File DB storage
+class Ace():
+    @staticmethod
+    def open_db_from_file(absolut_path):
+        _dir=path.dirname(absolut_path)
+        if not path.isdir(_dir):
+            log_aos.error("No such file or direcory: %s" % _dir)
+            return None
         try:
             f = open(absolut_path, 'r+b')
         except:
             f = open(absolut_path, 'w+b')
         return f
 
-    def low_write(self, btreeIO, variable: str, value: str) -> bool:
+    @staticmethod
+    def sync(btreeIO) -> None:
+        btreeIO.flush()
+        log_aos.debug("Sync %s to underlaying stream" % btreeIO)
+
+    @staticmethod
+    def low_write(btreeIO, variable: str, value: str) -> bool:
+        if not getattr(btreeIO, 'put', False): raise AceError("btreeIO closed")
         try:
             btreeIO.put(variable, str(value))
         except Exception as e:
@@ -46,82 +51,87 @@ class Ace_of_States_Basis():
             return False
         else:
             return True
-    def low_read(self, btreeIO, variable: str) -> str|None:
+
+    @staticmethod
+    def low_read(btreeIO, variable: str) -> str|None:
+        if not getattr(btreeIO, 'get', False): raise AceError("btreeIO closed")
         value = btreeIO.get(str(variable))
         if value: return value.decode('utf-8')
         return None
-    def sync(self, btreeIO) -> None:
-        btreeIO.flush()
-        log_aos.debug("Sync IO %s" % btreeIO)
-    def _sync_all_persistant(self) -> None:
-        for f in self.PERSISTANT_FILES:
-            self.sync(self.PERSISTANT_FILES[f])
 
-    ###############################################################
-    ### Temporary
-    ###############################################################
-    def temp_created(func):
-        """Decorator which checks that the IO buffer is available"""
-        def creation_checking(self, db_label, variable, value):
-            if not db_label is self.VAULT:
-                if not db_label in self.TEMP_IO_STREAMS:
-                    log_aos.debug("[%s] Create btree IO instance" % db_label)
-                    self.TEMP_IO_STREAMS.update({db_label: btree.open(uio.BytesIO())})
-            return func(self, db_label, variable, value)
-
-        return creation_checking
-
-    @temp_created
-    def write(self, db_label:str, variable:str, value:str) -> bool:
-        """Write to temporary storage"""
-        if self.read(db_label, variable) == value:
-            log_aos.debug("[%s] Variable \"%s\" is already \"%s\"" % (db_label, variable, value))
-            return True
-        if db_label is self.VAULT:
-            log_aos.debug("[%s] Write \"%s\" to \"%s\"" % ("VAULT", value, variable))
-            return self.low_write(db_label, variable, value)
+class AOS():
+    def __init__(self, file = None) -> None:
+        self.VAULT = None
+        if file:
+            fd = Ace.open_db_from_file(file)
+            if not fd:
+                log_aos.error("No such file or direcory: %s" % file)
+                raise DirNotFoundError("No such file or direcory: %s" % file)
+            self.VAULT = btree.open(Ace.open_db_from_file(file))
         else:
-            log_aos.debug("[%s] Write \"%s\" to \"%s\"" % (db_label, value, variable))
-            return self.low_write(self.TEMP_IO_STREAMS[db_label], variable, value)
+            self.VAULT = btree.open(uio.BytesIO())
 
-    def read(self, db_label:str, variable:str, default:str = None) -> str|None:
-        """Read from temporary storage"""
-        if db_label is self.VAULT:
-            value = self.low_read(db_label, variable)
-        else:
-            if not db_label in self.TEMP_IO_STREAMS:
-                log_aos.debug("Label \"%s\" not in TEMP_IO_STREAMS" % db_label)
-                return default
-            value = self.low_read(self.TEMP_IO_STREAMS[db_label], variable)
-        return value if value else default
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.sync()
+
+    def register_sync(self):
+        SignalHandler.register(2, Ace.sync, self.VAULT)
+        SignalHandler.register(15, Ace.sync, self.VAULT)
+        atexit(partial(Ace.sync, self.VAULT))
+        log_aos.debug("Register sync at exit")
+
+    def write(self, variable: str, value: str, datatype = None) -> bool:
+        """
+        TODO добавить сохранение типа с помощью служебной переменной в базе
+        напр. переменная dict с value = {'a': 1, 'b': 2}
+        запишется как:
+        dict = "{'a': 1, 'b': 2}"
+        dict_type = "dict"
+        и это надо вернуть как dict({'a': 1, 'b': 2})
+        """
+        return Ace.low_write(self.VAULT, variable, value)
+
+    def read(self, variable: str, restore_type: bool = False):
+        """TODO выше"""
+        return Ace.low_read(self.VAULT, variable)
+
+    def sync(self):
+        Ace.sync(self.VAULT)
+
+    def close(self):
+        self.sync()
+        self.VAULT = None
 
     ###############################################################
     ### Math
     ###############################################################
     def transform_value(func):
         """ Декоратор, проверяет значения и трансформирует в int"""
-        def banana_transform(self, db_label, variable, value):
-            _old = self.read(db_label=db_label, variable=variable)
+        def banana_transform(self, variable, value):
+            _old = self.read(variable=variable)
             _old = _old if _old != None else 0
             _old = int(_old) if str(_old).lstrip('-').isdigit() else 0
             _new = int(value) if str(value).lstrip('-').isdigit() else 0
             
-            return func(self, db_label, variable, _new, old_value=_old)
+            return func(self, variable, _new, old_value =_old)
             
         return banana_transform
     @transform_value
-    def plus(self, db_label, variable, value:int, old_value:int=0):
+    def plus(self, variable, value:int, old_value:int=0):
         """ Прибавляет value к текущему значению variable """
         value = old_value + value
-        self.write(db_label=db_label, variable=variable, value=value)
+        self.write(variable=variable, value=value)
     @transform_value
-    def minus(self, db_label, variable, value:int, old_value:int=0):
+    def minus(self, variable, value:int, old_value:int=0):
         """ Вычитает value из текущего значения variable """
         value = old_value - value
-        self.write(db_label=db_label, variable=variable, value=value)
+        self.write(variable=variable, value=value)
 
     @transform_value
-    def add(self, db_label, variable, value:int, old_value:int=0):
+    def add(self, variable, value:int, old_value:int=0):
         """
         Добавляет к variable изменение(разность м/у новым и текущим) value.
         Если новое значение меньше - прибавляем полностью.
@@ -130,10 +140,10 @@ class Ace_of_States_Basis():
             value = old_value + value
         else:
             value = old_value + (value - old_value)
-        self.write(db_label=db_label, variable=variable, value=value)
+        self.write(variable=variable, value=value)
 
     @transform_value
-    def extremum(self, db_label, variable, value:int, old_value:int=0):
+    def extremum(self, variable, value:int, old_value:int=0):
         """
         Добавляет к variable разность м/у новым и текущим значением value
         если новое значение превышает текущее. В противном случае оставляем без изменений.
@@ -143,101 +153,25 @@ class Ace_of_States_Basis():
         else:
             log_aos.debug("Cant update extremum. Value of \"%s\" from \"%s\" is less then current" % (variable, db_label))
             return False
-        self.write(db_label=db_label, variable=variable, value=value)
+        self.write(variable=variable, value=value)
 
     @transform_value
-    def collect(self, db_label, variable, value:int, old_value:int=0):
+    def collect(self, variable, value:int, old_value:int=0):
         """
         Собирает (коллекционирует) значение метрики, прибавляя дельту м/у старыми
         и новыми данными. Учитывает ситуации, когда собираемая метрика обнуляется и счёт начинается с нуля. 
         """
         adj_variable = 'adj_{}'.format(variable)
-        adj_value = self.read(db_label=db_label, variable=adj_variable)
+        adj_value = self.read(variable=adj_variable)
         adj_value = adj_value if adj_value != None else 0
         adj_value = int(adj_value) if str(adj_value).isdigit() else 0
         # adj_value уже получен, записываем новые данные
-        self.write(db_label=db_label, variable=adj_variable, value=value)
+        self.write(variable=adj_variable, value=value)
         if value >= adj_value:
             # Собираемые данные равномерно растут, прибавляем дельту от новой и предыдущей метрики
             value = old_value + (value - adj_value)
-            self.write(db_label=db_label, variable=variable, value=value)
+            self.write(variable=variable, value=value)
         elif value < adj_value:
             # Собираемый объект был обнулён, отсчёт начат сначала
             value += old_value
-            self.write(db_label=db_label, variable=variable, value=value)
-
-    ###############################################################
-    ### Other
-    ###############################################################
-    @staticmethod
-    def safe_restore_type(typeclass, data: str):
-        if type(data) != str:
-            if isinstance(data, typeclass):
-                return data
-        try:
-            return typeclass(data)
-        except (ValueError):
-            log_aos.warning("Something went wrong with %s(%s)" % (typeclass, data))
-        return None
-
-    @staticmethod
-    def restore_type(typeclass: str, data: str):
-        if type(data) != str:
-            if isinstance(data, typeclass):
-                return data
-        else:
-            try:
-                ff = compile("%s(%s)" % (str(typeclass), str(data)), 'file', 'eval')
-                zz = eval(ff)
-                return zz
-            except (SyntaxError, ValueError):
-                log_aos.warning("Something went wrong with %s(%s)" % (typeclass, data))
-            except:
-                log_aos.warning("Failed %s type resoring of %s" % (data))
-        return False
-
-class Persistant(Ace_of_States_Basis):
-    def persistant_created(func):
-        """Decorator which checks that the DB file is available"""
-        def creation_checking(self, db_label, variable, value):
-            if not db_label is self.VAULT:
-                if not db_label in self.PERSISTANT_FILES:
-                    if not os_exists("%s/%s" % (PERSISTANT_DB_PATH, db_label)):
-                        if not os_exists(PERSISTANT_DB_PATH):
-                            os.mkdir(PERSISTANT_DB_PATH)
-                        log_aos.debug("[%s] Create btree IO instance" % db_label)
-                    self.PERSISTANT_FILES.update({db_label: btree.open(self.open_db_from_file("%s/%s" % (PERSISTANT_DB_PATH, db_label)))})
-            return func(self, db_label, variable, value)
-
-        return creation_checking
-
-    @persistant_created
-    def write(self, db_label, variable, value) -> bool:
-        """Write to persistant storage"""
-        if self.read(db_label, variable) == value:
-            log_aos.debug("[%s] Variable \"%s\" is already \"%s\"" % (db_label, variable, value))
-            return True
-        if db_label is self.VAULT:
-            log_aos.debug("[%s] Write \"%s\" to \"%s\"" % ("VAULT", value, variable))
-            return self.low_write(db_label, variable, value)
-        else:
-            log_aos.debug("[%s] Write \"%s\" to \"%s\"" % (db_label, value, variable))
-            return self.low_write(self.PERSISTANT_FILES[db_label], variable, value)
-
-    def read(self, db_label:str, variable:str, default:str = None) -> str|None:
-        """Read from persistant storage"""
-        if db_label is self.VAULT:
-            value = self.low_read(db_label, variable)
-        else:
-            if not db_label in self.PERSISTANT_FILES:
-                # but
-                if os_exists("%s/%s" % (PERSISTANT_DB_PATH, db_label)):
-                    self.PERSISTANT_FILES.update({db_label: btree.open(self.open_db_from_file("%s/%s" % (PERSISTANT_DB_PATH, db_label)))})
-                else:
-                    log_aos.debug("Label \"%s\" not in PERSISTANT_FILES" % db_label)
-                    return default
-            value = self.low_read(self.PERSISTANT_FILES[db_label], variable)
-        return value if value else default
-
-class Temporary(Ace_of_States_Basis):
-    pass
+            self.write(variable=variable, value=value)
--- __main__.py
+++ __main__.py
@@ -1,29 +1,34 @@
 #!/usr/bin/micropython
-import ace_of_states as ace
+from ace_of_states.multiple import Persistant, Temporary
 import ubus, logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger("AOS Service")
 
 """ Ace of States ubus service"""
-ACE_Persistant = ace.Persistant()
-ACE_Temporary = ace.Temporary()
+AOS_Persistant = Persistant()
+AOS_Temporary = Temporary()
 
-def write(handler, data):
-    persistant, label, variable, value =  data['persistant'], data['label'], data['variable'], data['value']
-    if persistant:
-        res = ACE_Persistant.write(label, variable, value)
-    else:
-        res = ACE_Temporary.write(label, variable, value)
+def write_persistant(handler, data):
+    label, variable, value = data['label'], data['variable'], data['value']
+    res = AOS_Persistant.write(label, variable, value)
     handler_reply(handler, {'Status': 'OK' if res else 'Fail'})
 
-def read(handler, data):
-    label, variable, persistant =  data['label'], data['variable'], data['persistant']
-    if persistant:
-        value = ACE_Persistant.read(label, variable)
-    else:
-        value = ACE_Temporary.read(label, variable)
+def read_persistant(handler, data):
+    label, variable = data['label'], data['variable']
+    value = AOS_Persistant.read(label, variable)
     handler_reply(handler, {variable: value})
+
+def write_temporary(handler, data):
+    label, variable, value = data['label'], data['variable'], data['value']
+    res = AOS_Temporary.write(label, variable, value)
+    handler_reply(handler, {'Status': 'OK' if res else 'Fail'})
+
+def read_temporary(handler, data):
+    label, variable = data['label'], data['variable']
+    value = AOS_Temporary.read(label, variable)
+    handler_reply(handler, {variable: value})
+
 
 def drop(handler, data):
     """DROPDATABASE"""
@@ -34,19 +39,28 @@ def handler_reply(handler, reply):
     handler.reply(reply)
 
 ubus.connect('/var/run/ubus.sock')
-ubus.add("aos", {
-    "write": {"method": write, "signature": {
-        "persistant": ubus.BLOBMSG_TYPE_BOOL,
+ubus.add("aos_persistant", {
+    "write": {"method": write_persistant, "signature": {
         "label": ubus.BLOBMSG_TYPE_STRING,
         "variable":  ubus.BLOBMSG_TYPE_STRING,
         "value":  ubus.BLOBMSG_TYPE_STRING
         }},
-    "read": {"method": read, "signature": {
-        "persistant": ubus.BLOBMSG_TYPE_BOOL,
+    "read": {"method": read_persistant, "signature": {
         "label": ubus.BLOBMSG_TYPE_STRING,
         "variable":  ubus.BLOBMSG_TYPE_STRING
-        }} #,
-    # "drop": {"method": drop, "signature": {}}
+        }}
+    }
+)
+ubus.add("aos_temporary", {
+    "write": {"method": write_temporary, "signature": {
+        "label": ubus.BLOBMSG_TYPE_STRING,
+        "variable":  ubus.BLOBMSG_TYPE_STRING,
+        "value":  ubus.BLOBMSG_TYPE_STRING
+        }},
+    "read": {"method": read_temporary, "signature": {
+        "label": ubus.BLOBMSG_TYPE_STRING,
+        "variable":  ubus.BLOBMSG_TYPE_STRING
+        }}
     }
 )
 try:
